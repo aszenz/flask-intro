0f016873753f8f0ffa264858be53fef4c3bb7854
aszen <aszenz@gmail.com>
2018-11-13 14:07:14 +0530

    using template strings

--- simple.py
+++ simple.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask import render_template_string
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,7 +10,21 @@ def hello_world():
         </html>
         """
     return html 
-
+@app.route('/template')
+def hello_user():
+    user = "asrar"
+    html = """
+            <html>
+                <h1>Welcome {{user}}</h1>
+                <ul>
+                    {% for color in colors %}
+                        <li>{{color}}</li>
+                    {% endfor %}
+                </ul>
+            </html>
+            """
+    rendered_html = render_template_string(html, user=user, colors=['red', 'green', 'blue'])
+    return rendered_html
 if __name__ == '__main__':
     app.debug=True
     app.run()
