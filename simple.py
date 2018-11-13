5d48305e8bf1a8035302cfe263ae188673e523f4
aszen <aszenz@gmail.com>
2018-11-13 13:51:59 +0530

    Return html from flask server

--- simple.py
+++ simple.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello, world!"
+    html= """
+        <html>
+            <h1>Serving from flask</h1>
+        </html>
+        """
+    return html 
 
 if __name__ == '__main__':
     app.debug=True
