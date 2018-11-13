485e012773388e2dae3c0766a123ec26309bb398
aszen <aszenz@gmail.com>
2018-11-13 15:19:55 +0530

    Route arguments and Custom error pages

--- simple.py
+++ simple.py
@@ -1,8 +1,5 @@
-from flask import Flask
-from flask import render_template_string
-from flask import render_template
+from flask import Flask, render_template_string, render_template, abort
 app = Flask(__name__)
-
 @app.route('/')
 def hello_world():
     html= """
@@ -27,9 +24,25 @@ def hello_user():
     rendered_html = render_template_string(html, user=user, colors=['red', 'green', 'blue'])
     return rendered_html
 @app.route('/template')
-def hello_user22():
+def hello_temp_user():
     user = "asrar"
     return render_template('index.html', user=user, colors=['red', 'green', 'blue'])
+@app.route('/abstract')
+def abstract():
+    return render_template('abstract.html')
+@app.route('/add/<num1>/<num2>')
+def add(num1, num2):
+    return "Num1: {}, Num2: {}, Sum: {}".format(num1, num2, int(num1) + int(num2))
+@app.route('/color/<string:color_name>')
+def find_color(color_name):
+    colors = ['red', 'green', 'blue']
+    if color_name not in colors:
+        abort(404)
+    else:
+        return "Color Found"
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html')
 if __name__ == '__main__':
     app.debug=True
     app.run()
